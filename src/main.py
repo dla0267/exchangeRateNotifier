5d23968b0f48afac24f62870e199b25090a3d380
dla0267 <dla0267@gmail.com>
2022-08-03 20:14:40 -0700

    Use WebDriverManager which carries out the management(i.e. download, setup, and maintenance) of the web drivers required by selenium

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+
 
 def scrapeExchangeRateFromWebsite():
     # this approach doesn't work as the website checks if this is a bot or not.
@@ -8,14 +11,16 @@ def scrapeExchangeRateFromWebsite():
     # print(page.text)
     # soup = BeautifulSoup(page.content, "html.parser")
 
-    driver = webdriver.Chrome("//Users/yuunlim92/Downloads/chromedriver")
+    service = Service(executable_path = ChromeDriverManager().install())
+    driver = webdriver.Chrome(service = service)
+
     driver.get('https://www.bloomberg.com/quote/USDKRW:CUR')
     content = driver.page_source
     soup = BeautifulSoup(content, features="html.parser")
 
     results = soup.find(id="root")
     exchangeRates = results.find_all("span", class_="priceText__06f600fa3e")
-    currency=results.find(class_="companyName__1af0080d26")
+    currency = results.find(class_="companyName__1af0080d26")
     for exchangeRate in exchangeRates:
         print(currency.text + ": " + exchangeRate.text)
 
